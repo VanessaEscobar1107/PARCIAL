dcf7ce05b64fd921826c85557fffb4033cd0b20c
Vanessa Escobar <escobarvanessa210@gmail.com>
2025-03-17 17:35:54 -0500

    ejemplo reduce

--- EjemploReduce.exs
+++ EjemploReduce.exs
@@ -2,10 +2,10 @@
 defmodule EjemploReduce do
 
   def main do
-    IO.puts(Enum.reduce([2,4,6,8], 2,fn(x, acc) -> x + acc end))|> IO.inspect
+    IO.puts(Enum.reduce([2,4,6,8], 2,fn(x, acc) -> x + acc end))|> IO.inspect()
   end
 end
 
 EjemploReduce.main()
 # La salida es:
-#  20
+#  22
