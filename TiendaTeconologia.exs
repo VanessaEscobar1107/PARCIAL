e7b25ba115e7f26ee8c9171fc191f8310ef90ab6
Vanessa Escobar <escobarvanessa210@gmail.com>
2025-03-17 16:43:44 -0500

    actualizar tienda de tecnologia

--- TiendaTeconologia.exs
+++ TiendaTeconologia.exs
@@ -1,31 +1,59 @@
   # Crear un módulo llamado TiendaTecnologia que contenga los siguientes módulos:
   defmodule TiendaTecnologia do
-    defmodule Producto do
-      defstruct [:nombre, :precio, :stock]
-    end
+    def main do
+      # Crear un mapa llamado inventario que contenga los siguientes productos:
+      inventario = %{
+        "Laptop" => %{
+          "precio" => 1000,
+          "cantidad" => 5
+        },
+        "Mouse" => %{
+          "precio" => 20,
+          "cantidad" => 100
+        },
+        "Teclado" => %{
+          "precio" => 50,
+          "cantidad" => 50
+        }
+      }
+      # Crear una funcion que actualizar_inventario que reciba el nombre del producto y la cantidad a agregar y devuelva el inventario
+      def actualizar_inventario(producto, cantidad) do
+        IO.puts("Agrege el producto: #{producto} y la cantidad: #{cantidad}")
+        IO.gets("> ") # Se pide al usuario que ingrese el producto y la cantidad
+        IO.puts("Producto: #{producto} y cantidad: #{cantidad}")
 
-    
-    defmodule Cliente do
-      defstruct [:nombre, :apellido, :edad]
-    end
+        # Si el prodcuto no existe debe mostrar un mensaje que diga "El producto no existe"
+        if producto != inventario do
+          IO.puts("El producto no existe")
+        end
+      else
 
-    defmodule Venta do
-      defstruct [:cliente, :productos, :total]
-    end
+        # Se actualiza el inventario con el producto y la cantidad
+        inventario = %{
+          producto => %{
+            "precio" => 1000,
+            "cantidad" => 5
+          },
+          producto => %{
+            "precio" => 20,
+            "cantidad" => 100
+          },
+          producto => %{
+            "precio" => 50,
+            "cantidad" => 50
+          }
+        }
 
-    defmodule Tienda do
-      defstruct [:productos, :ventas]
-
-      def agregar_producto(tienda, producto) do
-        %{tienda | productos: [producto | tienda.productos]}
       end
 
-      def agregar_venta(tienda, venta) do
-        %{tienda | ventas: [venta | tienda.ventas]}
-      end
-
-      def calcular_total(venta) do
-        Enum.reduce(venta.productos, 0, fn producto, total -> producto.precio + total end)
+      # Si la cantidad a vender es mayor que la disponible debe mostrar un mensaje que diga "No hay suficiente cantidad"
+      if cantidad > inventario do
+        IO.puts("No hay suficiente cantidad")
       end
     end
+
+    # Mostrar el inventario actualizado con la cantidad de productos
+    IO.puts("Inventario actualizado: #{inventario}")
+
+
   end
