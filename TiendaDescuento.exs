526de72affba44a1a44a6d8ac9f98060a0c3c1eb
Vanessa Escobar <escobarvanessa210@gmail.com>
2025-03-17 21:54:37 -0500

    actualizar codigo tienda

--- TiendaDescuento.exs
+++ TiendaDescuento.exs
@@ -1,36 +1,55 @@
 defmodule TiendaDescuento do
 
-  def main do
-    # Implementar una funcion para calcular el descuento de un producto, recibe lista
-    # de productos y un porcentaje de descuento
-    def calcular_descuento(productos, descuento) do
-      # Se calcula el descuento de cada producto
-      for producto <- productos do
-        descuento_producto = producto["precio"] * descuento / 100
-        IO.puts("El descuento del producto #{producto["nombre"]} es: #{descuento_producto}")
-      end
+  # Funcion para calcular el descuento de un producto y mostrarlo
+  def calcular_descuento(productos, descuento) do
+    for productos <- productos do
+      descuento_producto = producto [:precio] * descuento / 100
+      IO.puts "El descuento del producto es: #{producto[:nombre]} es: #{descuento_producto}" # Mostrar el descuento del producto
     end
   end
 
-  # Calcular el precio con el descuento y generar una nueva lista usando  Enum.map
-  def calcular_precio_descuento(productos, descuento) do
-    Enum.map(productos, fn producto ->
-      descuento_producto = producto["precio"] * descuento / 100
-      precio_descuento = producto["precio"] - descuento_producto
-      %{producto | "precio" => precio_descuento}
+  # Funcion para calcular el precio con el descuento y generar una nueva lista usando Enum.map
+  def calculcar_precio_descuento(productos, descuento) do
+    Enum.map(productos, fn producto ->           # Enum.map para recorrer la lista de productos
+      descuento_producto = producto [:precio] * descuento / 100 # Calcular el descuento del producto
+      precio_descuento = producto [:precio] - descuento_producto # Calcular el precio con el descuento
+      %{producto | precio: precio_descuento}                     # Generar una nueva lista con el precio con descuento
     end)
   end
 
-  # Calcular el descuento aplicado con Enum.sum
+  # Funcion para calcular el descuento total aplicando con Enum.sum()
   def calcular_descuento_total(productos, descuento) do
-    Enum.sum(Enum.map(productos, fn producto ->
-      descuento_producto = producto["precio"] * descuento / 100
+    Enum.sum(Enum.map(productos, fn producto -> # Enum.map para recorrer la lista de productos
+    producto [:precio] * descuento / 100        # Calcular el descuento del producto y sumarlos con Enum.sum()
     end))
   end
 
-  # Si la lista de productos esta vacia debe retornar {[], 0}
-  def calcular_descuento_total([]), do: {[], 0}
-
+ # Si la lista de productos esta vacia, retorna {[],0}
+ def calcular_descuento_total([]), do: {[],0}  # Si la lista de productos esta vacia, retorna {[],0}
 end
+
+# Prueba del modulo
+productos = [
+  %{nombre: "Camisa", precio: 100},
+  %{nombre: "Pantalon", precio: 200},
+  %{nombre: "Zapatos", precio: 300}
+]
+
+# Calcular el descuento de los productos
+descuento = 10
+
+# Mostrar el descuento de los productos
+IO.puts("\n === Descuento individual de los productos ===")
+productos_descuento = TiendaDescuento.calcular_descuento(productos, descuento) # Calcular el descuento de los productos
+IO.inspect(productos_descuento) # Mostrar el descuento de los productos
+
+# Mostrar la lista con precios con descuento
+IO.puts("\n === Lista con precios con descuento ===")
+productos_descuento = TiendaDescuento.calculcar_precio_descuento(productos, descuento) # Calcular el precio con descuento
+IO.inspect(productos_descuento) # Mostrar la lista con precios con descuento
+
+# Mostrar el descuento total de los productos
+IO.puts("\n === Total de descuento aplicado ===")
+IO.puts("El total de descuento aplicado es: #{TiendaDescuento.calcular_descuento_total(productos, descuento)}") # Calcular el descuento total de los productos
 
 TiendaDescuento.main()
